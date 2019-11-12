a1195429c19ec0f7f746e35f070345e510969907
Fadhil <fadhildarma13@gmail.com>
2019-11-12 19:34:51 +0700

    fix pivot table

--- database/migrations/2019_10_25_104221_create_post_tag_table.php
+++ database/migrations/2019_10_25_104221_create_post_tag_table.php
@@ -15,8 +15,8 @@ class CreatePostTagTable extends Migration
     {
         Schema::create('posts_tags', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('post_id');
-            $table->integer('tag_id');
+            $table->integer('posts_id');
+            $table->integer('tags_id');
             $table->timestamps();
         });
     }
